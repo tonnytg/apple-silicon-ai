6ad843088397b687491ea2d4b23528955ca8922e
Antonio Thomacelli <tonnytg@gmail.com>
2024-09-26 00:05:48 -0300

    feat: add main.py to test

--- tensorflow/setup.sh
+++ tensorflow/setup.sh
@@ -1,3 +1,8 @@
-conda create env apple
-conda install pre-requs
-python -m pip install -e .
+conda create -n base-ai python=3.9
+conda activate base-ai
+conda install -c apple tensorflow-deps
+python -m pip install tensorflow-macos==2.9
+python -m pip install tensorflow-metal
+
+echo "Running Hello world"
+python main.py
